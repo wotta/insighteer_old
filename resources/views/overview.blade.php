341a8ea9e921dd4fa10cb545ef5f32fa6bbd6fe5
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-06-17 15:41:23 +0200

    Added account overview. - Added snippet to overview.

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -3,16 +3,85 @@
 @section('title', __('general.overview'))
 
 @section('content_header')
-	<h1>Dashboard</h1>
+    <h1>Dashboard</h1>
 @stop
 
 @section('content')
-	<div class="row">
-		<div class="col-md-8">
-			@include('partials/overview/budget/overview')
-		</div>
-		<div class="col-md-4">
-			@include('partials/overview/risk_meter/info_box')
-		</div>
-	</div>
+    <div class="row">
+        <div class="col-md-8">
+            @include('partials/overview/budget/overview')
+        </div>
+        <div class="col-md-4">
+            @include('partials/overview/risk_meter/info_box')
+
+            @include('partials.overview.accounts.overview')
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-success">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Bar Chart</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                    </div>
+                </div>
+                <div class="box-body">
+                    <div class="chart">
+                        <canvas id="barChart" style="height: 230px; width: 535px;" width="535" height="230"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
+
+@push('js')
+    <script>
+      $(function () {
+        //-------------
+        //- BAR CHART -
+        //-------------
+        var barChartCanvas                   = $('#barChart').get(0).getContext('2d')
+        var barChart                         = new Chart(barChartCanvas)
+        var barChartData                     = areaChartData
+        barChartData.datasets[1].fillColor   = '#00a65a'
+        barChartData.datasets[1].strokeColor = '#00a65a'
+        barChartData.datasets[1].pointColor  = '#00a65a'
+        var barChartOptions                  = {
+          //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+          scaleBeginAtZero        : true,
+          //Boolean - Whether grid lines are shown across the chart
+          scaleShowGridLines      : true,
+          //String - Colour of the grid lines
+          scaleGridLineColor      : 'rgba(0,0,0,.05)',
+          //Number - Width of the grid lines
+          scaleGridLineWidth      : 1,
+          //Boolean - Whether to show horizontal lines (except X axis)
+          scaleShowHorizontalLines: true,
+          //Boolean - Whether to show vertical lines (except Y axis)
+          scaleShowVerticalLines  : true,
+          //Boolean - If there is a stroke on each bar
+          barShowStroke           : true,
+          //Number - Pixel width of the bar stroke
+          barStrokeWidth          : 2,
+          //Number - Spacing between each of the X value sets
+          barValueSpacing         : 5,
+          //Number - Spacing between data sets within X values
+          barDatasetSpacing       : 1,
+          //String - A legend template
+          legendTemplate          : '<ul></ul>',
+          //Boolean - whether to make the chart responsive
+          responsive              : true,
+          maintainAspectRatio     : true
+        }
+
+        barChartOptions.datasetFill = false
+        barChart.Bar(barChartData, barChartOptions);
+      });
+    </script>
+@endpush
