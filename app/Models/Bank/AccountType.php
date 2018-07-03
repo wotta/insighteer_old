22666dec3cd6969d47f5e6e3554676423fd50c8f
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-07-03 22:18:47 +0200

    Add bank account
    - Added AccountType model.
    - Added Account model.

---
+++ app/Models/Bank/AccountType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Bank;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccountType extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'is_commercial',
+    ];
+}
