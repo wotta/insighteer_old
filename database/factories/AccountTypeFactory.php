f80a9f5951fd414340bca3dffd2f4c97f3ce0ea1
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-07-03 22:16:55 +0200

    Add bank account
    - Added account type factory.
    - Added account factory.

---
+++ database/factories/AccountTypeFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Bank\AccountType::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name(),
+        'description' => $faker->text(),
+        'is_commercial' => $faker->boolean(),
+    ];
+});
