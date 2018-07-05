729b282724b9ac447bb97dfe47a3719a80fc03dc
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-07-05 22:49:14 +0200

    Add bank account
    - Added LaravelBaseService.
    - Added LaravelBaseServiceInterface.

--- Insighteer/Services/LaravelBaseService.php
+++ Insighteer/Services/LaravelBaseService.php
@@ -3,18 +3,20 @@
 namespace Insighteer\Services;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Insighteer\Repositories\LaravelBaseRepositoryInterface;
 
 class LaravelBaseService implements LaravelBaseServiceInterface
 {
     /** @var LaravelBaseRepositoryInterface */
-    private $repository;
+    protected $repository;
 
-    public function __construct(LaravelBaseRepositoryInterface $repository)
+    public function __construct($repository)
     {
         $this->repository = $repository;
     }
 
-    public function all()
+    public function all(): Collection
     {
         return $this->repository->all();
     }
