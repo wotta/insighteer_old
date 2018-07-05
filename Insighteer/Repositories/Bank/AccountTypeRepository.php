e07d11e3a1a0ab0baa4877585acb8ee6f0a0b24c
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-07-05 22:55:02 +0200

    Add bank account
    - Updated AccountTypeRepository.
    - Updated AccountTypeRepositoryInterface.

--- Insighteer/Repositories/Bank/AccountTypeRepository.php
+++ Insighteer/Repositories/Bank/AccountTypeRepository.php
@@ -3,12 +3,20 @@
 namespace Insighteer\Repositories\Bank;
 
 use App\Models\Bank\AccountType;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Insighteer\Repositories\LaravelBaseRepository;
 
-class AccountTypeRepository extends LaravelBaseRepository
+class AccountTypeRepository extends LaravelBaseRepository implements AccountTypeRepositoryInterface
 {
     public function __construct(AccountType $accountType)
     {
-        parent::setModel($accountType);
+        $this->setModel($accountType);
+    }
+
+    public function setModel(Model $model): Model
+    {
+        $this->model = $model;
+        return $model;
     }
 }
---
+++ Insighteer/Repositories/Bank/AccountTypeRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Insighteer\Repositories\Bank;
+
+use Insighteer\Repositories\LaravelBaseRepositoryInterface;
+
+interface AccountTypeRepositoryInterface extends LaravelBaseRepositoryInterface
+{
+}
