283f736749a46e76c34df883cf7fa00ab8756da6
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-07-06 23:05:54 +0200

    Add bank account
    - Added account type overview partial.
    - Added _form view for account types.
    - Added index view for account types.

---
+++ resources/views/bank/account_type/_form.blade.php
@@ -0,0 +1,20 @@
+<form action="{{ $route }}" id="account_type_form" method="post" enctype="multipart/form-data">
+    @csrf
+    @method($method ?? 'post')
+
+    <div class="form-group">
+        <label for="name">Naam</label>
+        <input type="text" class="form-control" id="name" placeholder="Account type naam">
+    </div>
+
+    <div class="form-group">
+        <label for="description">Beschrijving</label>
+        <textarea class="form-control" name="description" id="description" cols="30" rows="3"></textarea>
+    </div>
+
+    <div class="checkbox">
+        <label>
+            <input type="checkbox" name="is_commercial"> Commercieel
+        </label>
+    </div>
+</form>
---
+++ resources/views/bank/account_type/partials/overview.blade.php
@@ -0,0 +1,29 @@
+<table class="table table-striped">
+    <tbody>
+        <tr>
+            <th>#</th>
+            <th>Type</th>
+            <th>Aantal</th>
+        </tr>
+        <tr>
+            <td>1.</td>
+            <td>Bank rekening</td>
+            <td><span class="badge bg-red">5</span></td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>Internet bankier rekening</td>
+            <td><span class="badge bg-yellow">70%</span></td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>Spaar rekening</td>
+            <td><span class="badge bg-light-blue">30%</span></td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>Spaar rekening student</td>
+            <td><span class="badge bg-green">90%</span></td>
+        </tr>
+    </tbody>
+</table>
