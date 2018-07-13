216e596a44b8f84467d06261c87f0d936ee2c6d5
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-07-13 21:14:35 +0200

    Add bank account
    - Import class.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,7 +12,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        \App\User::create([
+        User::create([
             'name' => 'wotty',
             'email' => 'wotty@test.com',
             'password' => bcrypt('test12'),
