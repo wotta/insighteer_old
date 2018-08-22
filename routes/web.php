fa840aea9d46a684cdbc18ed13f48753c0e4711a
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-08-22 20:43:36 +0200

    Updated some stuff to make sure nova could be added.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,3 @@
 <?php
 
-Route::middleware('auth')->group(function () {
-    Route::get('/', 'OverviewController@index')->name('overview');
-    Route::get('/home', 'HomeController@index')->name('home');
-});
-
-Route::namespace('Bank')->prefix('bank')->middleware('auth')->group(function () {
-    Route::resource('account-types', 'AccountTypeController');
-});
+Route::redirect('/', '/administration');
