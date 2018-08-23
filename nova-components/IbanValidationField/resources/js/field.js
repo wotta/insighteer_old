40613f30b45dccae48e3c4655cd7b91a1c0fea64
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-08-23 20:44:36 +0200

    Added basis iban validation resource tool.

--- nova-components/IbanValidationField/resources/js/field.js
+++
@@ -1,5 +0,0 @@
-Nova.booting((Vue, router) => {
-    Vue.component('index-iban-validation-field', require('./components/IndexField'));
-    Vue.component('detail-iban-validation-field', require('./components/DetailField'));
-    Vue.component('form-iban-validation-field', require('./components/FormField'));
-})
