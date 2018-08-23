302b2f7c83dc1c7706a6b171023db2912d0a81de
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-08-23 19:44:34 +0200

    Added SoftDeletes on the Address and Company models.

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Address extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'address',
         'second_address',
--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
     ];
