72805d0608cc67a3697fca5e28ea2b88439b54f6
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-09-15 00:42:04 +0200

    Fix use statements.

--- app/Nova/Metrics/Payments/PaymentsAmount.php
+++ app/Nova/Metrics/Payments/PaymentsAmount.php
@@ -4,6 +4,7 @@ namespace App\Nova\Metrics\Payments;
 
 use App\Models\Payments\Payment;
 use Illuminate\Http\Request;
+use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Metrics\Trend;
 
 class PaymentsAmount extends Trend
