bd7f0ba72c0212496082fd5ac209e538b62afac4
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-09-14 08:56:20 +0000

    Apply fixes from StyleCI

--- app/Nova/Payments/Payment.php
+++ app/Nova/Payments/Payment.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\Date;
-use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
-use Wotta\SyncCard\SyncCard;
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
 {
