645c4a50de5c632d11b5b6d68a372df7491abf9c
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-09-14 23:22:56 +0200

    Added 2 new migrations and fixed an old migration where the down method didn't work.

---
+++ database/migrations/2018_09_14_204738_update_balances_relation_to_payment_on_payments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateBalancesRelationToPaymentOnPaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropForeign(['balance_id']);
+            $table->dropColumn('balance_id');
+
+            $table->unsignedInteger('account_id')->after('id');
+
+            $table->foreign('account_id')
+                ->references('id')
+                ->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropForeign(['account_id']);
+            $table->dropColumn('account_id');
+
+            $table->unsignedInteger('balance_id')->after('id');
+
+            $table->foreign('balance_id')
+                ->references('id')
+                ->on('balances');
+        });
+    }
+}
---
+++ database/migrations/2018_09_14_210012_add_balance_columns_on_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBalanceColumnsOnAccountsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::dropIfExists('balances');
+
+        Schema::enableForeignKeyConstraints();
+
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->integer('amount')->after('iban');
+            $table->integer('previous_amount')->after('amount');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropColumn([
+                'amount',
+                'previous_amount'
+            ]);
+        });
+
+        (app()->make(CreateBalancesTable::class))->up();
+    }
+}
