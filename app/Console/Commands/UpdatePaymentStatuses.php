c304f4066b4736c2cd23653c65976d66cf093c50
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-09-20 22:16:29 +0200

    fix the command

--- app/Console/Commands/UpdatePaymentStatuses.php
+++ app/Console/Commands/UpdatePaymentStatuses.php
@@ -5,34 +5,34 @@ namespace App\Console\Commands;
 use App\Models\Payments\Payment;
 use App\Models\Status;
 use Illuminate\Console\Command;
-use Illuminate\Database\Eloquent\Collection;
 
 class UpdatePaymentStatuses extends Command
 {
-    /** @var Collection */
-    private $payments;
-
     protected $signature = 'payments:status:update';
 
     protected $description = 'Update the payment statuses.';
 
     public function __construct(Payment $payments)
     {
-        parent::__construct();
-
-        $this->payments = $payments::whereDate('payment_date', '<=', now())->get();
+        parent::__construct();;
     }
 
     public function handle()
     {
-        $progressBar = $this->output->createProgressBar($this->payments->count());
+        $payments = Payment::whereDate('payment_date', '<=', now())->whereHas('status', function ($query) {
+            return $query->where('name', 'open');
+        })->get();
 
-        $this->payments->each(function (Payment $payment) use ($progressBar) {
-            $payment->status()->associate(Status::where('name', 'paid')->firstOrFail())->save();
+        if ($payments->count() > 0) {        
+            $progressBar = $this->output->createProgressBar($payments->count());
 
-            $progressBar->advance();
-        });
+            $payments->each(function (Payment $payment) use ($progressBar) {
+                $payment->status()->associate(Status::where('name', 'paid')->firstOrFail())->save();
 
-        $progressBar->finish();
+                $progressBar->advance();
+            });
+
+            $progressBar->finish();
+        }
     }
 }
