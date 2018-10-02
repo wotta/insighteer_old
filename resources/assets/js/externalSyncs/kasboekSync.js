0de88ec9ba8d21b0fa568acc8a4e8d72937c06ad
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-10-02 18:40:59 +0200

    Fix indents.

--- resources/assets/js/externalSyncs/kasboekSync.js
+++ resources/assets/js/externalSyncs/kasboekSync.js
@@ -26,40 +26,40 @@ dotEnv.config();
         });
       });
 
-      await page.click('#tblSubmenu_BR_BR > tbody > tr:nth-child(2) > td > a');
+    await page.click('#tblSubmenu_BR_BR > tbody > tr:nth-child(2) > td > a');
 
-      await page.waitForSelector('.panel');
+    await page.waitForSelector('.panel');
 
-      const data = await page.evaluate(() => {
-          const elements = Array.from(document.querySelectorAll('[id^="divOverzichtSaldo"]'));
+    const data = await page.evaluate(() => {
+        const elements = Array.from(document.querySelectorAll('[id^="divOverzichtSaldo"]'));
 
-          return elements.map(e => {
-              const data = {};
+        return elements.map(e => {
+            const data = {};
 
-              data.paymentDate = e.querySelector('div:nth-child(1)').innerText.trim();
-              data.amount = e.querySelector('div:nth-child(4)').innerText.trim();
+            data.paymentDate = e.querySelector('div:nth-child(1)').innerText.trim();
+            data.amount = e.querySelector('div:nth-child(4)').innerText.trim();
 
-              if (! data.amount) {
-                data.amount = e.querySelector('div:nth-child(5)').innerText.trim();
-              }
+            if (! data.amount) {
+              data.amount = e.querySelector('div:nth-child(5)').innerText.trim();
+            }
 
-              const parent = e.querySelector('div:nth-child(2)');
-              const strong = parent.querySelector('strong');
+            const parent = e.querySelector('div:nth-child(2)');
+            const strong = parent.querySelector('strong');
 
-              data.company = '';
+            data.company = '';
 
-              if (strong) {
-                  data.company = strong.innerText.trim();
-                  parent.removeChild(strong);
-              }
+            if (strong) {
+                data.company = strong.innerText.trim();
+                parent.removeChild(strong);
+            }
 
-              data.description = parent.innerText.trim();
+            data.description = parent.innerText.trim();
 
-              return data;
-          });
-      });
+            return data;
+        });
+    });
 
-      console.log(JSON.stringify(data));
+    console.log(JSON.stringify(data));
 
     await browser.close();
   } catch (e) {
